94449b8a855366b47881023d1ad8a57406b838ae
Vi L <vileal.88@gmail.com>
2021-10-14 20:48:59 -0300

    version uno

---
+++ empezar.js
@@ -0,0 +1,13 @@
+//inicializando variables
+let fName = ' Viviana ';
+let lastName = 'Leal ';
+var age = 25;
+//concatenamos en una sola variable
+var fullName = fName + lastName + age;
+//mostramos
+console.log(fullName)
+ age = age + 2;
+//concatenar
+var fullName = fName + lastName + age;
+//mostrar
+console.log(fullName);
